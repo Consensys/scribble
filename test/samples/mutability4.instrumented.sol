3e73f5988698d176289217789a5955601a4234f7
blitz-1306 <pavel@maddevs.io>
2021-04-07 23:27:15 +0500

    Reorganize test samples to reduce amount of compiler invocation time

--- test/samples/mutability4.instrumented.sol
+++ test/samples/mutability4.instrumented.sol
@@ -1,16 +1,34 @@
 pragma solidity 0.6.10;
 
-contract Foo {
+/// Utility contract holding a stack counter
+contract __scribble_ReentrancyUtils {
+    bool __scribble_out_of_contract = true;
+}
+/// invariant {:msg ""} x > 0;
+contract Foo is __scribble_ReentrancyUtils {
     event AssertionFailed(string message);
+
+    struct vars1 {
+        bool __scribble_check_invs_at_end;
+    }
+
+    struct vars2 {
+        bool __scribble_check_invs_at_end;
+    }
 
     uint internal x = 1;
 
     function getX() public returns (uint RET_0) {
+        vars1 memory _v;
+        _v.__scribble_check_invs_at_end = __scribble_out_of_contract;
+        __scribble_out_of_contract = false;
         RET_0 = _original_Foo_getX();
         if (!(x > 0)) {
-            emit AssertionFailed("0: ");
+            emit AssertionFailed("1: ");
             assert(false);
         }
+        if (_v.__scribble_check_invs_at_end) __scribble_check_state_invariants();
+        __scribble_out_of_contract = _v.__scribble_check_invs_at_end;
     }
 
     function _original_Foo_getX() private view returns (uint) {
@@ -22,18 +40,38 @@ contract Foo {
     }
 
     function getXPlus2() public returns (uint RET_0) {
+        vars2 memory _v;
+        _v.__scribble_check_invs_at_end = __scribble_out_of_contract;
+        __scribble_out_of_contract = false;
         RET_0 = _original_Foo_getXPlus2();
         if (!(x > 2)) {
-            emit AssertionFailed("1: ");
+            emit AssertionFailed("2: ");
             assert(false);
         }
+        if (_v.__scribble_check_invs_at_end) __scribble_check_state_invariants();
+        __scribble_out_of_contract = _v.__scribble_check_invs_at_end;
     }
 
     function _original_Foo_getXPlus2() private returns (uint) {
         return getXPlus1() + 1;
     }
-}
-/// Utility contract holding a stack counter
-contract __scribble_ReentrancyUtils {
-    bool __scribble_out_of_contract = true;
+
+    /// Check only the current contract's state invariants
+    function __scribble_Foo_check_state_invariants_internal() internal {
+        if (!(x > 0)) {
+            emit AssertionFailed("0: ");
+            assert(false);
+        }
+    }
+
+    /// Check the state invariant for the current contract and all its bases
+    function __scribble_check_state_invariants() virtual internal {
+        __scribble_Foo_check_state_invariants_internal();
+    }
+
+    constructor() public {
+        __scribble_out_of_contract = false;
+        __scribble_check_state_invariants();
+        __scribble_out_of_contract = true;
+    }
 }
