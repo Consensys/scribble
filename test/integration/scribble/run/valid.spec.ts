1c8b3e8ef6807763b50eb2fe23a6d19818ca6d37
blitz-1306 <hyperx27@gmail.com>
2021-11-01 13:03:10 +0500

    Added macro samples and intergated them to run/valid test suite to indicate if macros are working properly

--- test/integration/scribble/run/valid.spec.ts
+++ test/integration/scribble/run/valid.spec.ts
@@ -15,7 +15,12 @@ describe(`Command "scribble <filename>" is working properly`, () => {
         ["contract_pos.sol", ["--debug-events", "--no-assert"]],
         ["if_assigned_complex.sol", ["--debug-events", "--no-assert"]],
         ["contract_multi_arg_debug.sol", ["--debug-events", "--no-assert"]],
-        ["dbg_event_tests.sol", ["--debug-events", "--no-assert"]]
+        ["dbg_event_tests.sol", ["--debug-events", "--no-assert"]],
+
+        /**
+         * Macros
+         */
+        ["simple_macro.sol", ["--macro-path", "test/samples/macros/ownable.scribble.yaml"]]
     ]);
 
     it(`Instrumented source samples are present in ${samplesDir}`, () => {
