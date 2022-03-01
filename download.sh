d7d90bb83926dc358e2a17fed23f7f606db901c0
blitz-1306 <hyperx27@gmail.com>
2022-03-01 22:55:35 +0500

    Updated download script

--- download.sh
+++ download.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+if [[ $# -ne 2 ]] ; then 
+    echo "The $0 utility requires two input arguments:"
+    echo "- Arch for compilers to download. See https://github.com/ethereum/solc-bin for available archs."
+    echo "- Target directory to store the downloaded compilers."
+    echo ""
+    echo "Note that to use custom compiler cache path, the env variable SOL_AST_COMPILER_CACHE should be set."
+    echo "See https://github.com/ConsenSys/solc-typed-ast/blob/master/README.md for the details."
+    echo ""
+    echo "Example:"
+    echo ""
+    echo "$0 'wasm' path/to/compiler/cache"
+    echo "export SOL_AST_COMPILER_CACHE=path/to/compiler/cache"
+
+    exit 1;
+fi
+
 ARCH="${1}"
 CACHE_DIR="${2}"
 TARGET_DIR="$CACHE_DIR/$ARCH"
