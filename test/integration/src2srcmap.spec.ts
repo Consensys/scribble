10879442305f129a28eb39891b603a4e62bb8d9b
blitz-1306 <hyperx27@gmail.com>
2023-04-28 11:40:31 +0500

    Bumped dependencies. Remove sharde code, now available in solc-typed-ast.

--- test/integration/src2srcmap.spec.ts
+++ test/integration/src2srcmap.spec.ts
@@ -19,6 +19,7 @@ import {
     MemberAccess,
     OverrideSpecifier,
     ParameterList,
+    parseBytecodeSourceMapping,
     PragmaDirective,
     SourceUnit,
     StructuredDocumentation,
@@ -86,41 +87,6 @@ function fragment(src: string, contents: string) {
     const [off, len] = parseSrcTriple(src);
 
     return contents.slice(off, off + len);
-}
-
-type DecodedBytecodeSourceMapEntry = {
-    byteIndex: number;
-    start: number;
-    length: number;
-    sourceIndex: number;
-    jump: string;
-};
-
-function parseBytecodeSourceMapping(sourceMap: string): DecodedBytecodeSourceMapEntry[] {
-    return sourceMap
-        .split(";")
-        .map((chunk) => chunk.split(":"))
-        .map(([start, length, sourceIndex, jump]) => ({
-            start: start === "" ? undefined : start,
-            length: length === "" ? undefined : length,
-            sourceIndex: sourceIndex === "" ? undefined : sourceIndex,
-            jump: jump === "" ? undefined : jump
-        }))
-        .reduce(
-            ([previous, ...all], entry) => [
-                {
-                    start: parseInt(entry.start || previous.start, 10),
-                    length: parseInt(entry.length || previous.length, 10),
-                    sourceIndex: parseInt(entry.sourceIndex || previous.sourceIndex, 10),
-                    jump: entry.jump || previous.jump
-                },
-                previous,
-                ...all
-            ],
-            [{} as any]
-        )
-        .reverse()
-        .slice(1);
 }
 
 describe("Src2src map test", () => {
